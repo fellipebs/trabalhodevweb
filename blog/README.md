0fedf7ab201b5f21dac0afd5ac27e6a323b19aaf
Giusier <giusier@hotmail.com>
2022-06-05 13:40:24 -0300

    listagem de posts

--- blog/README.md
+++ blog/README.md
@@ -1,6 +1,3 @@
 # TODO
 
-- Deixar funcional o crud de Posts (DELETE);
-- Listagem de posts por categoria;
-- Listagem de posts recentes
 - Implementar likes
--- blog/src/main/java/com/example/blog/controller/Controller.java
+++ blog/src/main/java/com/example/blog/controller/Controller.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.blog.service.PostService;
-
+import com.example.blog.model.Post;
 import com.example.blog.model.Usuario;
 import com.example.blog.service.CategoriaService;
+
+import java.util.Collections;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -23,7 +27,11 @@ public class Controller {
     public ModelAndView index(@SessionAttribute(name = "usuarioAtual", required = false) Usuario usuarioAtual) {
         ModelAndView mav = new ModelAndView("index");
 
-        mav.addObject("allPosts", postService.getAllPost());
+        List<Post> posts = postService.getAllPost();
+        // ordena pelos ultimos posts
+        Collections.reverse(posts);
+
+        mav.addObject("allPosts", posts);
         mav.addObject("allCategorias", categoriaService.getAllCategoria());
 
         mav.addObject("info_usuario", usuarioAtual != null && usuarioAtual.getNome() != null ? usuarioAtual : null);
